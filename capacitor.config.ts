3add3e52ad15fe4793f62fd324f5432a4a59e0f2
Marcos-Zero <mmartinezr@alumno.iplacex.cl>
2024-01-09 21:06:53 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.prueba3',
+  appName: 'prueba3',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
